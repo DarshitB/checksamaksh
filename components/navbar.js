cf26e8977d65e20bb87d6ae0f5cf3c135c6ad984
darshit patel <darshitbhanderi2316@gmail.com>
2022-05-27 09:34:46 +0530

    check

---
+++ components/navbar.js
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import styles from "../styles/Nev.module.css";
+
+function Navbar() {
+  return (
+    <>
+      <div className={styles.main_wrapper_nev}>
+        <h1>jdhfjsgdfjhdsv</h1>
+        <h1>jdhfjsgdfjhdsv</h1>
+        <h1>jdhfjsgdfjhdsv</h1>
+        <h1>jdhfjsgdfjhdsv</h1>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import Head from "next/head";
+import Image from "next/image";
+import Navbar from "../components/navbar";
+
+export default function Home() {
+  return (
+    <div className="main_container">
+      <Head>
+        <title>Samaksh Trade Consultants</title>
+        <meta
+          name="description"
+          content="we provide web and app services, Software Services, Business Marketing(B2B,B2E), Digital Marketing, Video Marketing, Search Engine Optimization(SEO), Graphics Design, Business To Customer Services(B2C)"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+        hdsgfvsdhgfhgsdvfhsvdfjhbsdjhfbsd4
+        <Navbar />
+      </div>
+    </div>
+  );
+}
